9a57af0fc5b2d5f099604643885955ac29afb397
soeque1 <soeque1@gmail.com>
2018-07-08 01:00:19 +0900

    added prev version of docker

--- docker/build_dockerfile.sh
+++ docker/build_dockerfile.sh
@@ -4,3 +4,6 @@ img_ver=0.1.0
 docker build -t bench_gpu:$img_ver -f bench_gpu/Dockerfile ./
 docker build -t bench_cpu:$img_ver -f bench_cpu/Dockerfile ./
 docker build -t bench_mkl:$img_ver -f bench_mkl/Dockerfile ./
+
+docker build -t bench_gpu_prev:$img_ver -f bench_gpu_prev/Dockerfile ./
+docker build -t bench_mkl_prev:$img_ver -f bench_mkl_prev/Dockerfile ./
