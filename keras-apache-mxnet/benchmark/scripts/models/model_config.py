0d4e0ca539680a161d5d2d2af5c7476f078559ee
soeque1 <soeque1@gmail.com>
2018-07-07 12:01:36 +0900

    added mnist_mlp

--- keras-apache-mxnet/benchmark/scripts/models/model_config.py
+++ keras-apache-mxnet/benchmark/scripts/models/model_config.py
@@ -4,6 +4,7 @@ Script modified from TensorFlow Benchmark repo:
 https://github.com/tensorflow/benchmarks/blob/keras-benchmarks/scripts/keras_benchmarks/models/model_config.py
 """
 from models import resnet50_benchmark
+from models import mnist_mlp_benchmark
 from models import resnet50_benchmark_tf_keras
 from models import lstm_synthetic
 from models import lstm_text_generation
@@ -12,6 +13,9 @@ from models import lstm_text_generation
 def get_model_config(model_name):
     if model_name == 'resnet50':
         return resnet50_benchmark.Resnet50Benchmark()
+
+    if model_name == 'mnist_mlp':
+        return lstm_text_generation.MnistMlpBenchmark()
 
     if model_name == 'resnet50_tf_keras':
         return resnet50_benchmark_tf_keras.Resnet50Benchmark()
